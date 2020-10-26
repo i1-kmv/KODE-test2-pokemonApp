f5c1919d00e8bc9275caf26363404a4f89ad4543
i1-kmv <i1-kmv@mail.ru>
2020-10-26 21:04:59 +0200

    Updates

---
+++ precache-manifest.f6bb6345cf626bccb95d2e402a3e1e33.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056c333657aca1604ee2172aff9c0531",
+    "url": "/KODE-test2-pokemonApp/index.html"
+  },
+  {
+    "revision": "9ca4a34d9d5723f64296",
+    "url": "/KODE-test2-pokemonApp/static/css/main.7d6301ab.chunk.css"
+  },
+  {
+    "revision": "eb3bf7fae94d90ef913c",
+    "url": "/KODE-test2-pokemonApp/static/js/2.582c3e5b.chunk.js"
+  },
+  {
+    "revision": "44d582a97d02337b347d751d7baba8fd",
+    "url": "/KODE-test2-pokemonApp/static/js/2.582c3e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca4a34d9d5723f64296",
+    "url": "/KODE-test2-pokemonApp/static/js/main.9f743864.chunk.js"
+  },
+  {
+    "revision": "7df0b77853c6782c1433",
+    "url": "/KODE-test2-pokemonApp/static/js/runtime-main.e2b4698e.js"
+  },
+  {
+    "revision": "81c42c842631eeac77b9411be40e5b16",
+    "url": "/KODE-test2-pokemonApp/static/media/App-bg.81c42c84.jpg"
+  },
+  {
+    "revision": "99b57ac6b051b807765a4e33a7b3b0ef",
+    "url": "/KODE-test2-pokemonApp/static/media/PNF.99b57ac6.jpg"
+  },
+  {
+    "revision": "6b4ffa55fdd7e087c6990685887572b4",
+    "url": "/KODE-test2-pokemonApp/static/media/go.6b4ffa55.svg"
+  }
+]);
